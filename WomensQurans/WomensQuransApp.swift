9caab8f0237d5d7403cb1e2f4cf39a310bf381c7
Esha <esharashidd@hotmail.com>
2026-02-02 17:31:59 +0700

    Initial Commit

---
+++ WomensQurans/WomensQuransApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomensQuransApp.swift
+//  WomensQurans
+//
+//  Created by Esha Rashid on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WomensQuransApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
